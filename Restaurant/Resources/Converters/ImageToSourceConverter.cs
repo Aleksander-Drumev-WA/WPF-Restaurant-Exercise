ee4740b9ab52f8557a630e41c20fde05933caf0c
Aleksander-Drumev-WA <Aleksander.Drumev@waveaccess.global>
2022-04-19 14:31:41 +0300

    Exported default path in image converter into a variable.

--- Restaurant/Resources/Converters/ImageToSourceConverter.cs
+++ Restaurant/Resources/Converters/ImageToSourceConverter.cs
@@ -7,9 +7,11 @@ namespace WPF_Restaurant.Resources.Converters
 {
 	public class ImageToSourceConverter : IValueConverter
 	{
+		private static readonly string defaultPath = Environment.CurrentDirectory + "\\Resources\\Images\\not-found-image.jpg";
+
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			var result = string.Empty;
+			string? result = null;
 			if (value is string path)
 			{
 				var exists = File.Exists(path);
@@ -17,17 +19,9 @@ namespace WPF_Restaurant.Resources.Converters
 				{
 					result = path;
 				}
-				else
-				{
-					result = Environment.CurrentDirectory + "\\Resources\\Images\\not-found-image.jpg";
-				}
-			}
-			else
-			{
-				result = Environment.CurrentDirectory + "\\Resources\\Images\\not-found-image.jpg";
 			}
 
-			return result;
+			return result ?? defaultPath;
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
