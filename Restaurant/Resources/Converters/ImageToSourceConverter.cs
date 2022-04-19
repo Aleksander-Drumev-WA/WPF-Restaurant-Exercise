594e8bdc9d4093b1090d4fe06d36192dd36327a2
Aleksander-Drumev-WA <Aleksander.Drumev@waveaccess.global>
2022-04-19 14:00:56 +0300

    Load default image if original image fails to load.

---
+++ Restaurant/Resources/Converters/ImageToSourceConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace WPF_Restaurant.Resources.Converters
+{
+	public class ImageToSourceConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var result = string.Empty;
+			if (value is string path)
+			{
+				var exists = File.Exists(path);
+				if (exists)
+				{
+					result = path;
+				}
+				else
+				{
+					result = Environment.CurrentDirectory + "\\Resources\\Images\\not-found-image.jpg";
+				}
+			}
+			else
+			{
+				result = Environment.CurrentDirectory + "\\Resources\\Images\\not-found-image.jpg";
+			}
+
+			return result;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Binding.DoNothing;
+		}
+	}
+}
